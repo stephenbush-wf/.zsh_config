fc4bccc4105f86896b9a5f3a45b032835dcbd19f
Stephen Bush <stephen.bush@workiva.com>
2015-03-23 14:26:41 -0600

    Updated the documentation for TerminalVariables.sh, and added more robust error-catching

--- TerminalVariables.sh
+++ TerminalVariables.sh
@@ -6,23 +6,35 @@
 # Define and save commands unique to individual terminal IDs, to be run when 
 #   called at the appropriate time.  Allows for a single command to cause
 #   different
+# Terminal variables can have many uses, including but not limited to:
+#   -- The ability to define certain values with distinct to each terminal
+#   -- Programmatically set up different configuration settings for each terminal
+#   -- Create a list of commands to be executed that is unique to each terminal
+
 
 TVAR_CONFIG_FILE=$PWD"/.TERM_VARS.cfg"
 
+# Dependency Check
 which -s prependAlias &> /dev/null
 if [[ $? != 0 ]]; then
   echo "$fg[red]Error, this Script requires Stephen Bush's ConfigWriter.sh in order to function properly.  Please ensure that it is sourced in your .zshrc file prior to loading this script."
   return
 fi
 
-
+# Initialize config file if it doesnt exist
 if [[ ! ( -e $TVAR_CONFIG_FILE ) ]]; then
 	config -c $TVAR_CONFIG_FILE reset
 	local itemToLoad=$(echo "$TTY""_onLoad" | sed -e 's:/::g')
 	config -c $TVAR_CONFIG_FILE add -k $itemToLoad
 fi
 
+# setTerminalVariable <Keyname> <Value>
+# Set a variable for the current Terminal ID with the specified key/value
 function setTerminalVariable() {
+	if [[ $1 == "" ]]; then
+		echo "$fg[red]Error, setTerminalVariable requires at least one parameter, which is a variable keyname!"
+		return 11
+	fi
 	local itemToLoad=$(echo "$TTY" | sed -e 's:/::g')"_""$1"
 	eval "$itemToLoad""=""Empty"
 	config -c $TVAR_CONFIG_FILE load -k $itemToLoad
@@ -31,11 +43,15 @@ function setTerminalVariable() {
 	else 
 		config -c $TVAR_CONFIG_FILE set -kv $itemToLoad $2
 	fi
+	return 0
 }
 
+# getTerminalVariable <Keyname>
+# Retrieve the specified variable for the current Terminal ID with the specified key
 function getTerminalVariable() {
 	local itemToLoad=$(echo "$TTY" | sed -e 's:/::g')"_""$1"
 	# echo "Loading $itemToLoad"
 	config -c $TVAR_CONFIG_FILE load -k $itemToLoad
 	eval echo \$$itemToLoad
+	return 0
 }
